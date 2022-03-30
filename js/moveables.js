a9bc1ca384db5b5cca3f177aca12242cb39d56f5
adrobiso <adrobiso@users.noreply.github.com>
2022-03-31 06:38:04 +0900

    now with a db!

--- js/moveables.js
+++ js/moveables.js
@@ -1,146 +1,170 @@
-let moveables;
+const moveables = new Map();
+const target = { id: 'target1', targetMoveable: '' };
 
-window.onload = init;
+fetch('db.json')
+  .then(response => { return response.json(); })
+  .then(jsonData => init(jsonData));
 
-function init() {
-   let red = {label:'red', audioElement:document.getElementById('redAudio')};
-   let blue = {label:'blue', audioElement:document.getElementById('blueAudio')};
-   moveables = new Map([[blue.label, blue],[red.label, red]]);
+function init(db) {
+  const moveablesBox = document.getElementById('moveablesBox');
+  for (let colorData of db.colors) {
+    const newElement = createMoveableElement(colorData, moveablesBox);
+    moveables.set(newElement.htmlElement.id, newElement);
+  }
 }
 
-function drag_start(event) {
-   setTimeout(function () {
-      event.target.style.visibility = 'hidden';
-   }, 1);
-   const style = window.getComputedStyle(event.target, null);
-   event.dataTransfer.setData('text/plain',
-      event.target.id + ',' + (parseInt(style.getPropertyValue('left'), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue('top'), 10) - event.clientY));
+function createMoveableElement(data, parent) {
+  const element = document.createElement('div');
+  element.id = data.label;
+  element.className = 'moveable';
+  element.draggable = true;
+  element.addEventListener('dragstart', dragStart);
+  const square = document.createElement('div');
+  square.className = 'square';
+  square.style.backgroundColor = data.label;
+  element.appendChild(square);
+  const audio = document.createElement('audio');
+  audio.id = data.label + 'Audio';
+  audio.src = data.audioSrc;
+  element.appendChild(audio);
+  parent.appendChild(element);
+
+  return { htmlElement: element, audioElement: audio, data: data };
 }
 
-function drag_over(event) {
-   event.preventDefault();
+function dragStart(event) {
+  setTimeout(function () {
+    event.target.style.visibility = 'hidden';
+  }, 1);
+  const style = window.getComputedStyle(event.target, null);
+  event.dataTransfer.setData('text/plain',
+    event.target.id + ',' + (parseInt(style.getPropertyValue('left'), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue('top'), 10) - event.clientY));
+}
+
+function dragOver(event) {
+  event.preventDefault();
 }
 
 function drop(event) {
-   const data = event.dataTransfer.getData('text/plain').split(',');
-   const offset = data.slice(1);
-   const draggedElement = document.getElementById(data[0]);
-   setTimeout(function () {
-      draggedElement.style.visibility = 'visible';
-   }, 1);
-   draggedElement.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
-   draggedElement.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
-   event.preventDefault();
-   return draggedElement;
+  const data = event.dataTransfer.getData('text/plain').split(',');
+  const offset = data.slice(1);
+  const draggedElement = document.getElementById(data[0]);
+  setTimeout(function () {
+    draggedElement.style.visibility = 'visible';
+  }, 1);
+  draggedElement.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
+  draggedElement.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
+  event.preventDefault();
+  return draggedElement;
 }
 
-function reparent_on_drop(event) {
-   const draggedElement = drop(event);
-   event.currentTarget.appendChild(draggedElement);
-   setTimeout(function () {
-      draggedElement.style.left = '0px';
-      draggedElement.style.top = '0px';
-   }, 1);
+// function reparentOnDrop(event) {
+//   const draggedElement = drop(event);
+//   event.currentTarget.appendChild(draggedElement);
+//   setTimeout(function () {
+//     draggedElement.style.left = '0px';
+//     draggedElement.style.top = '0px';
+//   }, 1);
+// }
+
+// function recepticleOnDrop(event) {
+//   reparentOnDrop(event);
+//   const newItemButton = document.getElementById("newItem");
+//   const feeder = document.getElementById("feeder");
+//   if (feeder.children.length > 0) {
+//     newItemButton.disabled = false;
+//   }
+// }
+
+function checkAnswerEvent(event) {
+  if (checkAnswer()) {
+    event.currentTarget.disabled = true;
+  }
 }
 
-function recepticle_on_drop(event) {
-   reparent_on_drop(event);
-   const newItemButton = document.getElementById("newItem");
-   const feeder = document.getElementById("feeder");
-   if (feeder.children.length > 0) {
-      newItemButton.disabled = false;
-   }
+function checkAnswer() {
+  let isCorrect = false;
+  const moveableElements = document.getElementsByClassName('moveable');
+  for (let moveableElement of moveableElements) {
+    if (isObjOnObj(moveableElement, target)) {
+      if (moveableElement.id === target.targetMoveable) {
+        moveableElement.style.outline = '2px dashed lightgreen';
+        document.getElementById('newQuestion').disabled = false;
+        isCorrect = true;
+      } else { moveableElement.style.outline = '2px dashed red'; }
+    }
+    else { moveableElement.style.outline = 'none'; }
+  }
+  return isCorrect;
 }
 
-function check_answer_event(event) {
-   const target = document.getElementById('target1');
-   if (check_answer(target)) {
-      event.currentTarget.disabled = true;
-   }
-}
-
-function check_answer(targetElement) {
-   let isCorrect = false;
-   const moveableElements = document.getElementsByClassName('moveable');
-   for (let moveableElement of moveableElements) {
-      if (isObjOnObj(moveableElement, targetElement)) {
-         if (moveableElement.id == targetElement.dataset.targetMoveable) {
-            moveableElement.style.outline = '2px dashed lightgreen';
-            document.getElementById('newQuestion').disabled = false;
-            isCorrect = true;
-         } else { moveableElement.style.outline = '2px dashed red'; }
-      }
-      else { moveableElement.style.outline = 'none'; }
-   }
-   return isCorrect;
-}
-
-function toggle_lock(event) {
-   const parent = event.target.parentElement;
-   if (parent.getAttribute('draggable') == 'true') {
-      parent.setAttribute('draggable', 'false');
-   } else {
-      parent.setAttribute('draggable', 'true');
-   }
-}
+// function toggleLock(event) {
+//   const parent = event.target.parentElement;
+//   if (parent.getAttribute('draggable') === 'true') {
+//     parent.setAttribute('draggable', 'false');
+//   } else {
+//     parent.setAttribute('draggable', 'true');
+//   }
+// }
 
 function isObjOnObj(a, b) {
-   const al = a.offsetLeft;
-   const ar = a.offsetLeft + a.offsetWidth;
-   const bl = b.offsetLeft;
-   const br = b.offsetLeft + b.offsetWidth;
+  const al = a.offsetLeft;
+  const ar = a.offsetLeft + a.offsetWidth;
+  const bl = b.offsetLeft;
+  const br = b.offsetLeft + b.offsetWidth;
 
-   const at = a.offsetTop;
-   const ab = a.offsetTop + a.offsetHeight;
-   const bt = b.offsetTop;
-   const bb = b.offsetTop + b.offsetHeight;
+  const at = a.offsetTop;
+  const ab = a.offsetTop + a.offsetHeight;
+  const bt = b.offsetTop;
+  const bb = b.offsetTop + b.offsetHeight;
 
-   if (bl > ar || br < al) { return false; }//overlap not possible
-   if (bt > ab || bb < at) { return false; }//overlap not possible
+  if (bl > ar || br < al) { return false; }//overlap not possible
+  if (bt > ab || bb < at) { return false; }//overlap not possible
 
-   return true;
+  return true;
 }
 
-function new_question(event) {
-   const moveableElements = document.getElementsByClassName('moveable');
-   for (let moveableElement of moveableElements) {
-      moveableElement.style.outline = 'none';
-      moveableElement.style.position = '';
-      moveableElement.style.left = 'initial';
-      moveableElement.style.top = 'initial';
-   }
-   setRandomTargets();
-   document.getElementById('checkAnswer').disabled = false;
-   event.target.disabled = true;
+function setNewQuestion(event) {
+  resetMoveables();
+  setRandomTarget();
+  document.getElementById('checkAnswer').disabled = false;
+  event.target.disabled = true;
 }
 
-function setRandomTargets() {
-   const targets = document.getElementsByClassName('target');
-   const moveableElements = document.getElementsByClassName('moveable');
-
-   for (let target of targets) {
-      let index = Math.floor(Math.random() * moveableElements.length);
-      let moveableElement = moveableElements[index];
-      target.dataset.targetMoveable = moveableElement.id;
-      play_audio(moveableElement.id);
-   }
+function resetMoveables() {
+  const moveableElements = document.getElementsByClassName('moveable');
+  for (let moveableElement of moveableElements) {
+    moveableElement.style.outline = 'none';
+    moveableElement.style.position = '';
+    moveableElement.style.left = 'initial';
+    moveableElement.style.top = 'initial';
+  }
 }
 
-function play_audio(moveableId) {
-   moveables.get(moveableId).audioElement.play();
+function setRandomTarget() {
+  let randomIndex = Math.floor(Math.random() * moveables.size);
+  let moveableId = Array.from(moveables.values())[randomIndex].htmlElement.id;
+  target.targetMoveable = moveableId;
+  playAudio(moveableId);
 }
 
-function play_audio_from_event(event) {
-   play_audio(event.currentTarget.id);
+function playAudio(moveableId) {
+  if (moveables.has(moveableId)) {
+    moveables.get(moveableId).audioElement.play();
+  }
 }
 
-function play_target_audio_from_event(event) {
-   play_audio(event.currentTarget.dataset.targetMoveable)
+// function playAudioFromEvent(event) {
+//   playAudio(event.currentTarget.id);
+// }
+
+function playTargetAudio() {
+  playAudio(target.targetMoveable)
 }
 
-function new_item(event) {
-   const newItem = document.getElementById('feeder').children[0];
-   newItem.style.visibility = 'visible';
-   play_audio(newItem.id);
-   event.currentTarget.disabled = true;
+function newItem(event) {
+  const newItem = document.getElementById('feeder').children[0];
+  newItem.style.visibility = 'visible';
+  playAudio(newItem.id);
+  event.currentTarget.disabled = true;
 }
